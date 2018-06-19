737ce2367a4ae2e04dfb40cd3e6a64d3eef05c93
Ben Hacker <bhacker1995@gmail.com>
2018-06-19 15:58:14 -0500

    Notes

---
+++ src/components/Header/Header.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+import '../App/App'
+import '../App/App.css'
+
+class Header extends Component {
+    render() {
+        return (
+            <header className="App-header">
+                <h1 className="App-title">My Stars App</h1>
+            </header>
+        );
+    }
+}
+
+export default Header;
